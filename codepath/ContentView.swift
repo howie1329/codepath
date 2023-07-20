2baa6d67e0d7ceb5784d7bb8c3225e66cc080f88
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-07-19 23:01:38 -0400

    adding picture of school into assets adding text field for information added button to toggle alert

--- codepath/ContentView.swift
+++ codepath/ContentView.swift
@@ -8,13 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum gradeLevel: String, CaseIterable{
+    case first = "First"
+    case second = "Second"
+    case third = "Third"
+    case forth = "Forth"
+    }
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var schoolName = ""
+    @State var schoolGradeLevel: gradeLevel = .first
+    @State var numberOfPets = 0
+    @State var toggle = true
+    @State var showAlert = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("WGU")
+                .resizable()
+                .frame(width: 200, height: 200)
+            HStack{
+                Text("First")
+                TextField("",text: $firstName)
+                    .textFieldStyle(.roundedBorder)
+            }
+            HStack{
+                Text("Last")
+                TextField("", text: $lastName)
+                    .textFieldStyle(.roundedBorder)
+            }
+            HStack{
+                Text("School")
+                TextField("", text: $schoolName)
+                    .textFieldStyle(.roundedBorder)
+            }
+            
+            Picker("", selection: $schoolGradeLevel) {
+                ForEach(gradeLevel.allCases, id:\.self){item in
+                    Text(item.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            HStack{
+                Text("Pets \(numberOfPets)")
+                Stepper("", value: $numberOfPets)
+            }
+            
+            Toggle("More Pets? ", isOn: $toggle)
+            
+            Button {
+                showAlert.toggle()
+            } label: {
+                Text("Introduce Self")
+            }
         }
+        .alert("My Introduction", isPresented: $showAlert, actions: {
+            Button {
+                showAlert.toggle()
+            } label: {
+                Text("Nice To Meet You")
+            }
+
+        }, message: {
+            Text("My name is \(firstName) \(lastName) and I attend \(schoolName). I am currently in my \(schoolGradeLevel.rawValue) year and I own \(numberOfPets) dogs. It is \(toggle.description) that I want more pets. ")
+        })
         .padding()
     }
 }
