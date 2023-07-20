8c3f496fb9271a46612fd08126c06486d18a56e5
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-07-19 21:42:01 -0400

    Initial Commit

---
+++ codepath/codepathApp.swift
@@ -0,0 +1,17 @@
+//
+//  codepathApp.swift
+//  codepath
+//
+//  Created by Howard Thomas on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct codepathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
